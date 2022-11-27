69e2dfabd5e8acf8a677818b520728d60a69b386
Artuom4ik <masterskayaigr123@gmail.com>
2022-11-27 11:51:41 +0300

    Создал функцию def main.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests 
 
-
-params = {
-    "qnTM": "",
-    "lang": "ru"
-    }
-locations = ["Аэропорт Шереметьево", "Лондон", "Череповец"]
-for location in locations:
-    url = f'https://wttr.in/{location}'
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    print(response.text)
+def main():
+    params = {
+        "qnTM": "",
+        "lang": "ru"
+        }
+    locations = ["Аэропорт Шереметьево", "Лондон", "Череповец"]
+    for location in locations:
+        url = f'https://wttr.in/{location}'
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        print(response.text)
+if __name__=="__main__":
+    main()
