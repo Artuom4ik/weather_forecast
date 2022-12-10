447fe21434d6ceb3b934ab9690b69fc2a614c014
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-10 13:53:53 +0300

    Оформил код по PEP8.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests 
 
+
 def main():
     params = {
         "qnTM": "",
         "lang": "ru"
-        }
+     }
     locations = ["Аэропорт Шереметьево", "Лондон", "Череповец"]
     for location in locations:
         url = f'https://wttr.in/{location}'
         response = requests.get(url, params=params)
         response.raise_for_status()
-        print(response.text)
+        print(response.text)        
 if __name__=="__main__":
     main()
