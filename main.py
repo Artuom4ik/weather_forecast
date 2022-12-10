d9a4fb72c8966a8522e9e386c0658222f181f792
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-10 13:55:43 +0300

    Сделал поправки.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
         url = f'https://wttr.in/{location}'
         response = requests.get(url, params=params)
         response.raise_for_status()
-        print(response.text)        
+        print(response.text)    
+        
+            
 if __name__=="__main__":
     main()
